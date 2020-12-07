bdb7abfe92f39bae9b10895360a96796f4db4ec1
Dominik Rutter <dominik.rutter@gmail.com>
2020-12-07 05:17:59 -0500

    call day2 in main

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <exception>
 #include "Days/Day1.hpp"
+#include "Days/Day2.hpp"
 
 int main() {
 	try {
 		Day1 day1;
+		Day2 day2;
 	} catch (std::exception e) {
 		std::cout << "EXCEPTION THROWN : \n" << e.what() << "" << std::endl;
 	}
